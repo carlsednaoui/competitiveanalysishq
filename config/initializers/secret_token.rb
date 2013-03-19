e8d3f02f05b61604e8d903bf937b54afdabddb96
carl sednaoui <carlsednaoui@gmail.com>
2013-03-18 21:37:35 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Competitiveanalysishq::Application.config.secret_token = '71b78cf5767b2f7406ab1e2139bac3b7ac6894cdfac7ced908cf291ab77c1197f0af69627013267a3ec7ecf4f9f3a695b54e85f825b8957c00cdd8d7c578d4af'
